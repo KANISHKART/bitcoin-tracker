51c5da10c56817417ee10ae09acec54cd771c213
kanishkar <kanishkrish123@gmail.com>
2024-05-16 14:12:48 +0100

    Conected to bitcoin network

--- bitcoin_tracker.py
+++ bitcoin_tracker.py
@@ -16,7 +16,7 @@ PORT= 8333
 # peer_ip_address is the bitcoin node 
 def version_payload(peer_ip_address):
     
-    version = 60002
+    version = 60002 # https://developer.bitcoin.org/reference/p2p_networking.html
     services = 1
     timestamp = int(time.time()) #Unix timestamp
     
@@ -48,14 +48,18 @@ def payload_message(magicNumber, command, payload):
 # so the value is retun as is
 def verack_message():
     return bytearray.fromhex("f9beb4d976657261636b000000000000000000005df6e0e2")
+              
 
+def getaddr_message():
+    # The payload for getaddr is empty
+    return b''
 
 # below is the starting point for the program
 if __name__ == '__main__':
     
     # constant values 
     magic_value = 0xd9b4bef9
-    peer_ip_address = '198.154.93.110' # derived from https://bitnodes.io/api/v1/nodes/leaderboard/?limit=100&page=1 
+    peer_ip_address = '185.26.99.171' # derived from https://bitnodes.io/api/v1/nodes/leaderboard/?limit=100&page=1 
     buffer_size = 1024
 
     # Establish Socket TCP Connection
@@ -66,27 +70,64 @@ if __name__ == '__main__':
     payload = version_payload(peer_ip_address)
     
     #The 'version_message' variable will have the complete header + payload
-    version_message = payload_message(magic_value, 'version', payload)
+    version_message = payload_message(magic_value,'version', payload)
     
     # 1. Start the handshake with bitcoin node with "version"
     s.send(version_message)
     response_data = s.recv(buffer_size) #max amount of data to be recived with thissingle call. so there won't be any big message recieved
+ 
     
-    
-    
-
-    print("Request:")
-    print((binascii.hexlify(version_message)))
-    print("Response:")
-    print(binascii.hexlify(response_data))
-    
+    # 2. Send verack after version
     s.send(verack_message())
     response_verack=s.recv(buffer_size)
     
-    print("Request:")
-    print((binascii.hexlify(version_message)))
-    print("Response:")
-    print(binascii.hexlify(response_data))
+    # # 3. 'get_addr' after verack
+    # getAddr_message= payload_message(magic_value,'getaddr', getaddr_message())
+    # s.send(getAddr_message)
+    
+    while(True):
+        response_addr=s.recv(buffer_size)
+        magic_number, command, payload_length, checksum = struct.unpack('<L12sL4s', response_addr[:24])
+
+        # # Extract the payload
+        # payload = response_verack[24:24 + payload_length]
+
+        # Print the decoded header fields
+        print("Magic Number:", hex(magic_number))
+        print("Command:", command)
+        print("Payload Length:", payload_length)
+        print("Checksum:", checksum.hex())
+
+        # Print the payload
+        print("Payload:", payload.hex())
+         
+        print("---------------------------------------------------------------------------------------------------------")
+        print()
+        
+    
+   
+    # print(response_addr[4:13])
+    # print(response_addr[13:17])
+    
+    # print(struct.unpack('<12s', response_addr[4:12]))
+     # Parse the header
+     
+    # print(len(response_addr))
+    # magic_number, command, payload_length, checksum = struct.unpack('<L12sL4s', response_addr[:24])
+
+    # # # Extract the payload
+    # # payload = response_verack[24:24 + payload_length]
+
+    # # Print the decoded header fields
+    # print("Magic Number:", hex(magic_number))
+    # print("Command:", command.decode('utf-8').strip('\x00'))
+    # print("Payload Length:", payload_length)
+    # print("Checksum:", checksum.hex())
+
+    # # Print the payload
+    # print("Payload:", payload.hex())
+    
+   
    
     # Close the TCP connection
     s.close()
