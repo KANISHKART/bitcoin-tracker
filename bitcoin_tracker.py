cd3cb8db99c91a52f3bb39a8470ccfe7ba822132
kanishkar <kanishkrish123@gmail.com>
2024-05-18 23:56:25 +0100

    Completed code

--- bitcoin_tracker.py
+++ bitcoin_tracker.py
@@ -3,14 +3,25 @@
 # Student No: D23124630
 
 import binascii
+import datetime
 import socket
 import time
 import random
 import struct
 import hashlib
 
+
+import logging
+ 
+# Configure logging to save to a file
+logging.basicConfig(filename='output.log', level=logging.INFO)
+
 # Mostly in bitcoin node the port will be 8333
 PORT= 8333
+
+# constant values 
+MAGIC_NUMBER = 0xd9b4bef9
+
 
 # Below version payload values is exracted from  https://en.bitcoin.it/wiki/Protocol_documentation#version
 # peer_ip_address is the bitcoin node 
@@ -25,7 +36,8 @@ def version_payload(peer_ip_address):
     addr_peer = struct.pack('>8s16sH', b'\x01', 
         bytearray.fromhex("00000000000000000000ffff") + socket.inet_aton("127.0.0.1"), PORT)
     nonce = random.getrandbits(64)
-    start_height = 843638
+    
+    start_height = 844006 # BITCOIN block id 
     
     # The below <LQQ26s26sQ16sL indicates the format of binary data. '<' indicates the little-endian byte order.
     # '26s' represents a string of 26 bytes similarly for all the numbers followed by symbol represents the type with that many bytes
@@ -50,10 +62,7 @@ def verack_message():
     return bytearray.fromhex("f9beb4d976657261636b000000000000000000005df6e0e2")
               
 
-def getaddr_message():
-    # The payload for getaddr is empty
-    return b''
-
+# This is used to parse the command type in header messages
 def safe_utf8_decode(byte_sequence):
     try:
         decoded_text = byte_sequence.decode('utf-8')
@@ -61,101 +70,330 @@ def safe_utf8_decode(byte_sequence):
     except UnicodeDecodeError:
         # If decoding fails, return the original byte sequence
         return byte_sequence
+
+def var_int(buffer, offset):
+    """Parse a Bitcoin variable integer"""
+    value = buffer[offset]
+    if value < 0xfd:
+        return value, 1
+    elif value == 0xfd:
+        return struct.unpack('<H', buffer[offset + 1:offset + 3])[0], 3
+    elif value == 0xfe:
+        return struct.unpack('<I', buffer[offset + 1:offset + 5])[0], 5
+    else:
+        return struct.unpack('<Q', buffer[offset + 1:offset + 9])[0], 9
+
+def parse_transaction(tx_payload):
+    offset = 0
+
+    # Parse version
+    version, = struct.unpack_from('<I', tx_payload, offset)
+    offset += 4
+   
+    # Check for optional flag
+    flag = None
+    if tx_payload[offset:offset + 2] == b'\x00\x01':
+        flag = tx_payload[offset:offset + 2]
+        offset += 2
+   
+
+    # Parse input count
+    tx_in_count, var_int_size = var_int(tx_payload, offset)
+    offset += var_int_size
+  
+
+    # Parse inputs
+    tx_ins = []
+    for _ in range(tx_in_count):
+        tx_in = {}
+        tx_in['previous_output'] = tx_payload[offset:offset + 32]
+        offset += 32
+        tx_in['index'], = struct.unpack_from('<I', tx_payload, offset)
+        offset += 4
+        script_length, var_int_size = var_int(tx_payload, offset)
+        offset += var_int_size
+        tx_in['signature_script'] = tx_payload[offset:offset + script_length]
+        offset += script_length
+        tx_in['sequence'], = struct.unpack_from('<I', tx_payload, offset)
+        offset += 4
+        tx_ins.append(tx_in)
+
+
+    # Parse output count
+    tx_out_count, var_int_size = var_int(tx_payload, offset)
+    offset += var_int_size
+    
+    # Parse outputs
+    tx_outs = []
+    for _ in range(tx_out_count):
+        tx_out = {}
+        tx_out['value'], = struct.unpack_from('<Q', tx_payload, offset)
+        offset += 8
+        script_length, var_int_size = var_int(tx_payload, offset)
+        offset += var_int_size
+        tx_out['pk_script'] = tx_payload[offset:offset + script_length]
+        offset += script_length
+        tx_outs.append(tx_out)
+   
+
+    # Parse lock_time
+    lock_time, = struct.unpack_from('<I', tx_payload, offset)
+    offset += 4
+  
+
+    # Return parsed transaction as a dictionary
+    transaction = {
+        'version': version,
+        'flag': flag,
+        'inputs': tx_ins,
+        'outputs': tx_outs,
+        'lock_time': lock_time
+    }
+
+    return transaction
+
+# Function to convert Unix timestamp to human-readable datetime
+def unix_timestamp_to_datetime(timestamp):
+    return datetime.datetime.fromtimestamp(timestamp).strftime('%d %B %Y at %H:%M')
+
+
+# Function to parse the block data and extract information
+def parse_block_data(block_data):
+    version = struct.unpack('<I', block_data[:4])[0]
+    prev_block_hash = block_data[4:36].hex()
+    merkle_root = block_data[36:68].hex()
+    print("original payload::",block_data[36:68])
+    timestamp = unix_timestamp_to_datetime(struct.unpack('<I', block_data[68:72])[0])
+    bits = struct.unpack('<I', block_data[72:76])[0]
+    nonce = struct.unpack('<I', block_data[76:80])[0]
+    txn_count = struct.unpack('<B', block_data[80:81])[0]
+    # transactions = []
+    txn_start = 81
+    # for _ in range(txn_count):
+    #     txn_value = struct.unpack('<Q', block_data[txn_start + 8:txn_start + 16])[0] / 10**8
+    #     transactions.append(txn_value)
+    #     txn_start += 64
+    return version, prev_block_hash, merkle_root, timestamp, bits, nonce, txn_count
+
+def padded_block_hash(original_hash):
+    reversed_bytes = bytes.fromhex(original_hash)[::-1] # Reverse byte order
+    padded_hex = reversed_bytes.hex().rjust(64, "0")    # Convert to hexadecimal with leading zeros
+    return padded_hex
+    
+# Function to create message payload
+def create_message_payload(command, payload=b''):
+    magic = struct.pack('<L', MAGIC_NUMBER)
+    command = command.ljust(12, b'\x00')
+    length = struct.pack('<L', len(payload))
+    checksum = hashlib.sha256(hashlib.sha256(payload).digest()).digest()[:4]
+    return magic + command + length + checksum + payload
+
+# Function to calculate the hash of the block data
+def calculate_block_hash(block_data):
+    block_hash = hashlib.sha256(hashlib.sha256(block_data).digest()).digest()
+    return block_hash[::-1].hex()
+
+def read_tx_block_messages(sock, expected_tx_count, hash):
+    buffer = b""
+    received_tx_count = 0
+
+    while received_tx_count < expected_tx_count:
+        data = sock.recv(100024)
+        buffer += data
+
+        while len(buffer) >= 24:  # Minimum size of a message header
+            # Extract the header
+            g_magic_number, g_command, g_payload_length, g_checksum = struct.unpack('<L12sL4s', buffer[:24])
+            command_name = safe_utf8_decode(g_command)
+            total_message_length = 24 + g_payload_length
+
+            if "tx" in str(command_name):
+                print("\nPacket Magic     :", hex(g_magic_number))
+                print("Command name       :", command_name)
+                print("Payload Length     :", g_payload_length)
+                print("Payload Checksum   :", g_checksum.hex())
+
+                # Process the transaction payload here
+                tx_payload = buffer[24:24 + g_payload_length]
+                print(parse_transaction(tx_payload))
+
+                
+            elif "block" in str(command_name):
+                print("##################### block details ##########################")
+                 # Parse the message header
+                magic_number, command, payload_length, checksum = struct.unpack('<L12sL4s', buffer[:24])
+
+                # Parse the message header
+                magic_number, command, payload_length, checksum = struct.unpack('<L12sL4s', buffer[:24])
+
+                print("Packet Magic         :", hex(magic_number))
+                print("Command name         :", command)
+                print("Payload Length       :", payload_length)
+                print("Payload Checksum     :", checksum.hex())
+                        
+                logging.info("Packet Magic         :%s", hex(magic_number))
+                logging.info("Command name         : block",)
+                logging.info("Payload Length       :%s", payload_length)
+                logging.info("Payload Checksum     :%s", checksum.hex())
+                
+                # Parse block data
+                version, prev_block_hash, merkle_root, timestamp, bits, nonce, txn_count = parse_block_data(buffer[24:])
+                
+                logging.info("Block Version         :%s", hex(version))
+                logging.info("Block Previous Hash   :%s", bytes.fromhex(prev_block_hash)[::-1].hex())
+                logging.info("Merkle Root       :%s", bytes.fromhex(merkle_root)[::-1].hex())
+                logging.info("Block timestamp     :%s", timestamp)
+                logging.info("Nonce     :%s", bits)
+                logging.info("Nonce     :%s", nonce)
+                
+                print("Block Version         :", hex(version))
+                print("Block Previous Hash   :", bytes.fromhex(prev_block_hash)[::-1].hex())
+                print("Merkle Root       :", bytes.fromhex(merkle_root)[::-1].hex())
+                print("Block timestamp    :", timestamp)
+                print("Bits     :", bits)
+                print("Nonce     :", nonce)
+                
+                # if calculated_hash == hash:
+                #     print("Hashes Match! Block is Valid.")
+                # else:
+                #     print("Hashes Do Not Match! Block is Invalid.")
+                
+            received_tx_count += 1
+            
+            # Remove the processed message from the buffer
+            buffer = buffer[total_message_length:]
+
     
 # below is the starting point for the program
 if __name__ == '__main__':
     
-    # constant values 
-    magic_value = 0xd9b4bef9
+    
     peer_ip_address = '185.26.99.171' # derived from https://bitnodes.io/api/v1/nodes/leaderboard/?limit=100&page=1 
     buffer_size = 1024
+    
+    block_height=input("Enter block hash : ")
 
     # Establish Socket TCP Connection
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     s.connect((peer_ip_address, PORT))
     
     print("\nInitiaiting protocol handshake...")
+    logging.info("\nInitiaiting protocol handshake...")
   
     #The 'payload' variable will hold the byte array of the body
     payload = version_payload(peer_ip_address)
     
     #The 'version_message' variable will have the complete header + payload
-    version_message = payload_message(magic_value,'version', payload)
+    version_message = payload_message(MAGIC_NUMBER,'version', payload)
     
     # 1. Start the handshake with bitcoin node with "version"
     s.send(version_message)
     response_data = s.recv(buffer_size) #max amount of data to be recived with thissingle call. so there won't be any big message recieved
  
     print("Version recieved from Bitcoin network")
+    logging.info("Version recieved from Bitcoin network")
     
     # 2. Send verack after version
     s.send(verack_message())
     response_verack=s.recv(buffer_size)
     
     print("Verack recieved from Bitcoin network")
+    logging.info("Verack recieved from Bitcoin network")
     
-    # 3. 'get_addr' after verack
-    getAddr_message= payload_message(magic_value,'pong', getaddr_message())
-    s.send(getAddr_message)
+    # # 3. 'get_addr' after verack
     
+    getdata_payload = create_message_payload(b'getdata', struct.pack('<I', 1) + b'\x02' + bytes.fromhex(block_height)[::-1])
+    s.sendall(getdata_payload)
+        
+        
+    # getAddr_message= payload_message(magic_value,'getdata', b'')
+    # s.send(getAddr_message)
     print("Handshake Successful")
+    logging.info("Handshake Successful")
     
-    print("Listening to inventory messages in bitcoin network...\n")
-    
+    print("Listening to inventory vectors to get transaction & block details in bitcoin network...\n")
+    logging.info("Listening to inventory vectors to get transaction & block details in bitcoin network...\n")
+       
     print("---------------------------------------------------------------------------------------------------------")
+    logging.info("---------------------------------------------------------------------------------------------------------\n")
+
+ 
     while(True):
         response_addr=s.recv(3072) # increased to accomodate more message from bitcoin network
-        magic_number, command, payload_length, checksum = struct.unpack('<L12sL4s', response_addr[:24])
+        if(len(response_addr)>24):
+            MAGIC_NUMBER, command, payload_length, checksum = struct.unpack('<L12sL4s', response_addr[:24])
 
-        # filtering command type
-        command_type= safe_utf8_decode(command)
-       
-        if('inv' in command_type):
-            # Print the decoded header fields
+            # filtering command type
+            command_type= safe_utf8_decode(command)
             
-           
-            print("Packet Magic         :", hex(magic_number))
-            print("Command name         :", command_type)
-            print("Payload Length       :", payload_length)
-            print("Payload Checksum     :", checksum.hex())
+            if('inv' in str(command_type) or 'block' in str(command)):
+                # Print the decoded header fields
             
-            # Print the inventory payloads
-            inv_payload= response_addr[24:]
-            
-            print("Inventory Details:",response_addr[24:],"\n")
-            
-            # Extract the count
-            count = struct.unpack('<B', inv_payload[:1])[0]  # '<B' is little-endian unsigned char
-            print(f'Count: {count}')
-            
-            # Initialize list to hold inventory vectors
-            inventory_vectors = []
-
-            # Offset to start of inventory vectors
-            offset = 1
-
-            # Each inventory vector is 36 bytes
-            vector_size = 36
-
-            for i in range(count):
-                # Extract type (4 bytes) and hash (32 bytes)
-                vector_data = inv_payload[offset:offset + vector_size]
-                vector_type = struct.unpack('<I', vector_data[:4])[0]  # '<I' is little-endian unsigned int
-                vector_hash = vector_data[4:]
+                print("Packet Magic         :", hex(MAGIC_NUMBER))
+                print("Command name         :", command_type)
+                print("Payload Length       :", payload_length)
+                print("Payload Checksum     :", checksum.hex())
                 
-                # Append to list
-                inventory_vectors.append((vector_type, vector_hash.hex()))
+                # logging.info("Packet Magic         :%s", hex(magic_number))
+                # logging.info("Command name         : inv",)
+                # logging.info("Payload Length       :%s", payload_length)
+                # logging.info("Payload Checksum     :%s", checksum.hex())
                 
-                # Update offset
-                offset += vector_size
+                
+                # Print the inventory payloads
+                inv_payload= response_addr[24:]
+                
+                # print("Inventory Details:",response_addr[24:],"\n")
+                
+                print("Inventory Details: \n")
+                logging.info("Inventory Details: \n")
+                
+                # Extract the count
+                count = struct.unpack('<B', inv_payload[:1])[0]  # '<B' is little-endian unsigned char
+                print(f'Count: {count}')
+                logging.info(f'Count: {count}')
+                
+                # Initialize list to hold inventory vectors
+                inventory_vectors = []
 
-            # Print the inventory vectors
-            for idx, (vector_type, vector_hash) in enumerate(inventory_vectors):
-                print(f'Inventory Vector {idx + 1}:')
-                print(f'  Type: {vector_type}')
-                print(f'  Hash: {vector_hash}')
+                # Offset to start of inventory vectors
+                offset = 1
+
+                # Each inventory vector is 36 bytes
+                vector_size = 36
+
+                for i in range(count):
+                    # Extract type (4 bytes) and hash (32 bytes)
+                    vector_data = inv_payload[offset:offset + vector_size]
+                    vector_type = struct.unpack('<I', vector_data[:4])[0]  # '<I' is little-endian unsigned int
+                    vector_hash = vector_data[4:]
+                    
+                    # Append to list
+                    inventory_vectors.append((vector_type, vector_hash.hex()))
+                    
+                    # Update offset
+                    offset += vector_size
+
                 
-            print("---------------------------------------------------------------------------------------------------------")
+                # Print the inventory vectors
+                for idx, (vector_type, vector_hash) in enumerate(inventory_vectors):
+                    print(f'Inventory Vector {idx + 1}:')
+                    logging.info(f'Inventory Vector {idx + 1}:')
+                    print(f'  Type: {vector_type}')
+                    print(f'  Hash: {vector_hash}')
+                    logging.info(f'  Type: {vector_type}')
+                    logging.info(f'  Hash: {vector_hash}')
+                    # to get transaction details for the inv vectors
+                s.send(payload_message(MAGIC_NUMBER, 'getdata', inv_payload))
+                read_tx_block_messages(s, count, vector_hash)
+                logging.info("---------------------------------------------------------------------------------------------------------")
+                print("---------------------------------------------------------------------------------------------------------")
+        else:
+            print("######################## Skipped ##########################")
+            logging.info("######################## Skipped ##########################")
+            print(response_addr)
+            logging.info(response_addr)
     s.close()
+
 
  
---
+++ redme.md
@@ -0,0 +1,22 @@
+D2314630 - Knaishkar Thirunvaukkarasu
+
+1. Run the code
+
+>> python bitcoun_tracker.py  
+
+2. Provide lastet block hash or height in the input 
+
+Link: https://www.blockchain.com/explorer/blocks/btc?page=1
+
+3. This is command line based viewer
+
+4. You will see a log file generated named "output.log" 
+
+5. All block details will be store in the log file also it is print in the output along with "tx" inv vector messages
+
+6. I was able to do everything expect for the no of transactions in the block part.
+
+7. If the code is not working kindly ping me in d23124630@mytudublin.ie
+
+
+![alt text](image.png)
